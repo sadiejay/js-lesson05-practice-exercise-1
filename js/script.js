967415d9ec2e8074a873cf1a4b4b7ef4e34d65d0
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-03 18:41:24 -0500

    Adds event listeeners to toggle and applies styles

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ var pets = document.querySelector(".pets");
 pets.addEventListener("mouseover", function(){
     pets.innerText = "20k";
 });
+
+var toggle = document.querySelector('#toggle');
+var ball = document.querySelector(".ball");
+var title = document.querySelector('h1');
+var body = document.querySelector('body');
+
+toggle.addEventListener
+("click", function(){
+if (body.classList.contains("light")){
+body.classList.remove('light')
+ball.classList.remove('move-right');
+title.innerText = "Stealth Maxie"
+} else {
+body.classList.add('light')
+ball.classList.add("move-right")
+title.innerText = "Party Maxie"
+}   
+}
+
+)
