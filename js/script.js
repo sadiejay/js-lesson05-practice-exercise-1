048b0a556335add41d181b9ce945bc914ee904d3
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-03 18:29:34 -0500

    Adds mouseover events to footer text

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var fans = document.querySelector('.fans');
+fans.addEventListener("mouseover", function(){
+fans.innerText = "123k";
+});
+
+
+var fish = document.querySelector('.fish');
+fish.addEventListener("mouseover", function(){
+    fish.innerText = "47k";
+});
+
+
+var pets = document.querySelector(".pets");
+pets.addEventListener("mouseover", function(){
+    pets.innerText = "20k";
+});
